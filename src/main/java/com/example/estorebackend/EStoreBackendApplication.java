15eb80686e20744aba6297cde35542c35c118a0c
pallapoo <121492872+pallapoo@users.noreply.github.com>
2023-07-26 22:29:42 +0530

    Update EStoreBackendApplication.java

--- src/main/java/com/example/estorebackend/EStoreBackendApplication.java
+++ src/main/java/com/example/estorebackend/EStoreBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EStoreBackendApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("spring.config.location", "application.properties");
+		//System.setProperty("spring.config.location", "application.properties");
 		SpringApplication.run(EStoreBackendApplication.class, args);
 	}
 
